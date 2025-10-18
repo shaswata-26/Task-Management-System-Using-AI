a131fa6d90dc6164de7fcbe8e846c336a56d18c4
Shaswata Panda <106397517+shaswata-26@users.noreply.github.com>
2025-10-18 15:09:55 +0530

    updated icors

--- backend/middleware/cors.js
+++ backend/middleware/cors.js
@@ -1,7 +1,7 @@
 const cors = require('cors');
 
 const corsOptions = {
-  origin: "*",
+  origin: 'https://task-management-system-using-ai-1.onrender.com', // ✅ your frontend domain
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
